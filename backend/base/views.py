ef1ab3e6650ba83448ec387b6c2d4de26433e32c
quin <g.joaquin000@gmail.com>
2023-10-01 17:02:51 -0700

    added json web token functionality

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,7 +3,23 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer
+
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data["username"] = self.user.username
+        data["email"] = self.user.email
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(["GET"])
@@ -19,6 +35,13 @@ def getRoutes(request):
         "/api/products/<update>/<id>/",
     ]
     return Response(routes)
+
+
+@api_view(["GET"])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
